5760514708ff2828a90ec563270010e87116b6a2
Robert Russo <rrusso44@gmail.com>
2015-12-03 22:24:27 -0500

    Server with Sockets
    It works!!! and with multiple connections

--- server.py
+++ server.py
@@ -1,14 +1,52 @@
-import socketserver
+import socket
+import sys
+from thread import *
 
-class TCPHandler(socketserver.BaseRequestHandler):
-	def handle(self):
-		self.data = self.request.recv(1024).strip()
-		print("{} wrote: ").format(self.client_address[0])
-		print(self.data)
-		self.request.sendall(self.data.upper())
+#Server Information
+HOST = ''
+PORT = 9999
 
-if __name__ == "__main__":
-	HOST, PORT = "localhost", 9999
+#Create Socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
 
-	server = socketserver.TCPServer((HOST, PORT), TCPHandler)
-	server.serve_forever()
+#Bind Socket
+try:
+	s.bind((HOST, PORT))
+except socket.error as msg:
+	print("Bind failed. Error Code : ", str(msg[0]), ' Message ', msg[1])
+	sys.exit()
+print("Socket bind complete")
+
+#Listen
+s.listen(10)
+print("Socket listening")
+
+#Handle Connections
+def clientthread(conn):
+	#Welcome
+	conn.send("Welcome to the server. Type something and hit enter\n")
+
+	#Connections
+	while True:
+
+		#Recieve
+		data = conn.recv(1024)
+		reply = "Okay..." + data
+		if not data:
+			break
+
+		#Reply
+		conn.sendall(reply)
+
+	#Close
+	conn.close()
+
+#Create Threads
+while 1:
+	conn, addr = s.accept()
+	print("Connected with ", addr[0], ":", str(addr[1]))
+	start_new_thread(clientthread, (conn,))
+
+#Close Socket
+s.close()
